fa6b729004010eaef11ecc2fe24e8ad0289400ac
benBen <benmbenjamin@gmail.com>
2017-08-21 14:17:52 -0400

    temp about

--- page.php
+++ page.php
@@ -20,7 +20,7 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				the_content();
+				get_template_part( 'template-parts/content', 'page' );
 
 			endwhile; // End of the loop.
 			?>
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,9 +10,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
@@ -22,29 +19,23 @@
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'om' ),
 				'after'  => '</div>',
 			) );
+
+			if (have_rows('content')) :
+			  $slide_count = 0;
+			  while (have_rows('content')) : the_row();
+			  	$title = get_sub_field('title');
+			  	$body = get_sub_field('body');
+			  ?>
+			  <?php if ($title) { ?>
+					<h1 class="text-center title uppercase"><?php echo $title ?></h1>
+		  	<?php } ?>
+  		  <?php if ($body) { ?>
+  				<?php echo $body ?>
+  	  	<?php } ?>
+			  <?
+			  endwhile;
+			endif;
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-				edit_post_link(
-					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'om' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
-					),
-					'<span class="edit-link">',
-					'</span>'
-				);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 </article><!-- #post-<?php the_ID(); ?> -->
